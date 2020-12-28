a321ce4fb2b329e7841cd13597d1048d918dbbec
NirmitSawant <sawantnirmit@gmail.com>
2020-12-28 15:33:58 +0530

    SplashScreen Added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:covidtracker/india/indiahomepage.dart';
 import 'package:covidtracker/pages/faqs.dart';
 import 'package:covidtracker/panels/mythbusters.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,7 +13,7 @@ void main() {
       fontFamily: 'Montserrat',
       primaryColor: primaryBlack,
     ),
-    home: HomePage(),
+    home: Splash(),
     routes: {
       "/india": (_) => new IndiaHomePage(),
       "/world": (_) => new HomePage(),
@@ -20,4 +21,23 @@ void main() {
       "/myths": (_) => new MythBusters(),
     },
   ));
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 5,
+      backgroundColor: Colors.white,
+      image: Image.asset("images/covid.png"),
+      photoSize: 100,
+      loaderColor: Colors.black,
+      navigateAfterSeconds: HomePage(),
+    );
+  }
 }
